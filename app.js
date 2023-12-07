5de9dd2589b46b2a26c5eb604cca3645ab8c3919
LincolnDuarte06 <lincolnduartebel@gmail.com>
2023-12-07 16:43:50 -0400

    amem

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 const produtos = [
-  {id:1,titulo: "Whey Protein", preco:"R$173",descricao:"100% Whey é um suplemento a base de proteína concentrada do soro do leite (Whey Protein Concentrate), matéria-prima elaborada com alta tecnologia, de elevado valor biológico",imagem: "Whey-Protein-Baunilha.png"},
+  {id:1,titulo: "teste", preco:"R$173",descricao:"100% Whey é um suplemento a base de proteína concentrada do soro do leite (Whey Protein Concentrate), matéria-prima elaborada com alta tecnologia, de elevado valor biológico",imagem: "Whey-Protein-Baunilha.png"},
   {id:2,titulo: "Barra de Proteina", preco:"R$17,75", descricao:"A Growth Protein Bar fornece uma boa quantidade de proteínas com alto valor biológico, possui alta quantidade de fibras, sacia a fome e fornece bons nutrientes.", imagem: "barra-de-proteina-caramelo.png"},
   {id:3,titulo: "Creatina", preco:"R$187,55", descricao:"A Growth Protein Bar fornece uma boa quantidade de proteínas com alto valor biológico, possui alta quantidade de fibras, sacia a fome e fornece bons nutrientes.", imagem: "creatina-growth.png"},
   {id:4,titulo: "Creatina",preco:"R$1000",descricao:"A Growth Protein Bar fornece uma boa quantidade de proteínas com alto valor biológico, possui alta quantidade de fibras, sacia a fome e fornece bons nutrientes.",imagem: "creatina-growth.png"},
   {id:5,titulo: "Creatina",preco:"R$1000",descricao:"A Growth Protein Bar fornece uma boa quantidade de proteínas com alto valor biológico, possui alta quantidade de fibras, sacia a fome e fornece bons nutrientes.",imagem: "creatina-growth.png"},
-  {id:6,titulo: "Barra",preco:"R$1000",descricao:"descrição do produto",imagem: "barra-de-proteina-caramelo.png"},
-  {id:7,titulo: "Whey",preco:"R$1000",descricao:"descrição do produto",imagem:  "Whey-Protein-Baunilha.png"},
-  {id:8,titulo: "creatina",preco:"R$1000",descricao:"descrição do produto",imagem: "creatina-growth.png"},
-  {id:9,titulo: "barra",preco:"R$1000",descricao:"descrição do produto",imagem: "barra-de-proteina-caramelo.png"},
-  {id:10,titulo: "whey",preco:"R$1000",descricao:"descrição do produto",imagem: "Whey-Protein-Baunilha.png"},
+  {id:6,titulo: "Barra",preco:"R$1000",descricao:"A Growth Protein Bar fornece uma boa quantidade de proteínas com alto valor biológico, possui alta quantidade de fibras, sacia a fome e fornece bons nutrientes.",imagem: "barra-de-proteina-caramelo.png"},
+  {id:7,titulo: "Whey",preco:"R$1000",descricao:"A Growth Protein Bar fornece uma boa quantidade de proteínas com alto valor biológico, possui alta quantidade de fibras, sacia a fome e fornece bons nutrientes.",imagem:  "Whey-Protein-Baunilha.png"},
+  {id:8,titulo: "creatina",preco:"R$1000",descricao:"A Growth Protein Bar fornece uma boa quantidade de proteínas com alto valor biológico, possui alta quantidade de fibras, sacia a fome e fornece bons nutrientes.",imagem: "creatina-growth.png"},
+  {id:9,titulo: "barra",preco:"R$1000",descricao:"A Growth Protein Bar fornece uma boa quantidade de proteínas com alto valor biológico, possui alta quantidade de fibras, sacia a fome e fornece bons nutrientes.",imagem: "barra-de-proteina-caramelo.png"},
+  {id:10,titulo: "whey",preco:"R$1000",descricao:"A Growth Protein Bar fornece uma boa quantidade de proteínas com alto valor biológico, possui alta quantidade de fibras, sacia a fome e fornece bons nutrientes.",imagem: "Whey-Protein-Baunilha.png"},
 ]
 function BuscarProdutosPorID(id){
   const produto = produtos.find(produto => produto.id == id);
